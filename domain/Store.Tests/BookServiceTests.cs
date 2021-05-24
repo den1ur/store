e562603fb3d3dd8a85db88ede7b0b8aeef2b1daf
unknown <dremfox01@gmail.com>
2021-05-25 00:28:35 +0300

    Realizovat' poisk po ISBN i avtory

---
+++ domain/Store.Tests/BookServiceTests.cs
@@ -0,0 +1,49 @@
+﻿using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Store.Tests
+{
+    public class BookServiceTests
+    {
+        [Fact]
+        public void GetAllByQuery_WithIsbn_CallsGetAllByIsbn()
+        {
+            var bookRepositoreyStub = new Mock<IBookRepository>();
+            bookRepositoreyStub.Setup(x => x.GetAllByIsbn(It.IsAny<string>()))
+                               .Returns(new[] { new Book(1, "", "", "") });
+
+            bookRepositoreyStub.Setup(x => x.GetAllByTitleOrAuthor(It.IsAny<string>()))
+                               .Returns(new[] { new Book(2, "", "", "") });
+
+            var bookService = new BookService(bookRepositoreyStub.Object);
+            var validIsbn = "ISBN 12345-67890";
+
+            var actual = bookService.GetAllByQuery(validIsbn);
+
+            Assert.Collection(actual, book => Assert.Equal(1, book.Id));
+        }
+
+        [Fact]
+        public void GetAllByQuery_WithAuthor_CallsGetAllByTitleOrAuthor()
+        {
+            var bookRepositoreyStub = new Mock<IBookRepository>();
+            bookRepositoreyStub.Setup(x => x.GetAllByIsbn(It.IsAny<string>()))
+                               .Returns(new[] { new Book(1, "", "", "") });
+
+            bookRepositoreyStub.Setup(x => x.GetAllByTitleOrAuthor(It.IsAny<string>()))
+                               .Returns(new[] { new Book(2, "", "", "") });
+
+            var bookService = new BookService(bookRepositoreyStub.Object);
+            var invalidIsbn = "12345-67890";
+
+            var actual = bookService.GetAllByQuery(invalidIsbn);
+
+            Assert.Collection(actual, book => Assert.Equal(2, book.Id));
+        }
+    }
+}
--- ifrastructure/Store.Memory/BookRepository.cs
+++ ifrastructure/Store.Memory/BookRepository.cs
@@ -7,14 +7,21 @@ namespace Store.Memory
     {
         private readonly Book[] books = new[]
         {
-            new Book(1, "Art Of Programming"),
-            new Book(2, "Refactoring"),
-            new Book(3, "C Programming Language "),
+            new Book(1, "ISBN 1234567890", "D. Knuth", "Art Of Programming"),
+            new Book(2, "ISBN 2222222222", "M. Fowler", "Refactoring"),
+            new Book(3, "ISBN 3333333322", "B. Kernighan, D. Ritchie", "C Programming Language "),
         };
 
-        public Book[] GetAllByTitle(string titlePart)
+        public Book[] GetAllByIsbn(string isbn)
         {
-            return books.Where(book => book.Title.Contains(titlePart))
+            return books.Where(book => book.Isbn == isbn)
+                        .ToArray();
+        }
+
+        public Book[] GetAllByTitleOrAuthor(string query)
+        {
+            return books.Where(book => book.Author.Contains(query)
+                                    ||book.Title.Contains(query))
                         .ToArray();
         }
     }
